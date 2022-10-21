7515d79fcf7d4158a5b7e7c89156b4a810ec658d
Dmitriy <zeybert90@mail.ru>
2022-10-21 23:36:36 +0300

    Star

---
+++ Задача 38/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+int [] Numers(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+            res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int Sum (int[] array)
+{
+    int minmax = 0;
+    int min = array[0];
+    int max = array[0];
+    for(int i = 1; i < array.Length; i++)
+    {
+        if(array[i] < min)
+         {
+            min = array[i];
+         }
+        
+        if  (array[i] > max) 
+        {
+            max = array[i];
+        }
+
+    }
+    minmax = max - min;
+    return minmax;
+}
+Console.Clear();
+Console.Write("Введите числа массива:  ");
+string array = Console.ReadLine();
+int [] numers = Numers(array);
+int nums = Sum(numers);
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным числом массива => {nums}");
+Console.WriteLine();
---
+++ Задача со звездочкой/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача "со звездочкой": Разобраться с алгоритмом сортировки методом пузырька. Реализовать невозрастающую сторировку.
+int [] Numers(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+            res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+void Mas(int [] array)
+{   
+    Console.WriteLine();
+    Console.Write($"{"["}");
+    int num = 0;
+    for(int i = 0; i < array.Length-1; i++)
+    {
+        for(int n = i + 1; n < array.Length; n++)
+        {
+            if (array[i] < array[n])
+            {
+                num = array[i];
+                array[i] = array[n];
+                array[n] = num;
+            }
+        }
+
+    }
+    Console.Write(String.Join(", ", array));
+    Console.Write($"{"]"}");
+    Console.WriteLine();
+
+}
+
+Console.Clear();
+Console.Write("Введите числа массива: ");
+string numers = Console.ReadLine();
+int [] num = Numers(numers);
+Mas(num);
+Console.WriteLine();
